6ff5fba61990feae6f3294d6a8a9ca2916038838
Soongyu Kwon <me@soongyu.me>
2024-08-31 05:37:37 +0900

    test

--- lib/channel_talk_flutter.dart
+++ lib/channel_talk_flutter.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'channel_talk_flutter_platform_interface.dart';
 
 enum Appearance {
@@ -34,8 +32,8 @@ enum ChannelButtonPosition {
 
 class ChannelButtonOption {
   final ChannelButtonPosition position;
-  final double xMargin;
-  final double yMargin;
+  final int xMargin;
+  final int yMargin;
 
   ChannelButtonOption({
     required this.position,
